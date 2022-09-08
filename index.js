aa0df5a9cdc037ac309b455eed66146dc851a4ac
mattis - Helife <mattis.dalleau@epitech.eu>
2022-09-08 18:03:31 +0400

    Fix: Forgot to move calendar

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const settings = require("./settings.json");
     const calendar_file = await intra.getCalendarFile(settings.calendar);
     calendar_file.pipe(fs.createWriteStream("calendar.ical"));
 
-    const cmd = `rm -rf calendar_folder && git clone ${settings.url} --depth=1 calendar_folder && cd calendar_folder && git add -A && (git commit -am "update" && git push) || exit 0`
+    const cmd = `rm -rf calendar_folder && git clone ${settings.url} --depth=1 calendar_folder && mv calendar.ical calendar_folder && cd calendar_folder && git add -A && (git commit -am "update" && git push) || exit 0`
 
     exec(cmd);
 
